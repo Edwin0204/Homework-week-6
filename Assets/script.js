802faed2f6970d0912eef6ff512703f175a8ffbf
Edwin0204 <edwins0204@gmail.com>
2022-05-26 18:01:38 -0500

    Temperature for 6 days

--- Assets/script.js
+++ Assets/script.js
@@ -5,9 +5,15 @@ function GetInfo() {
     cityName.innerHTML =newName.value;
     let Key = "777b995858df96d8a63d276827cb2cf5";
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${newName.value}&units=metric&appid=${Key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${newName.value}&units=metric&appid=${Key}`)
     .then(res => res.json()).then(data => {
         console.log(data);
+
+            for(i = 0; i<6; i++){
+                document.getElementById("day" + (i+1) + "temperature").innerHTML = "Temp: " + (data.list[i].main.temp)+ "°";
+    
+            }
+
 
     });
     
