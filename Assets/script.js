0b43661ce2ee000c13c35569bcb26d04b3d0782f
Edwin0204 <edwins0204@gmail.com>
2022-05-26 17:31:04 -0500

    Tested api conection

--- Assets/script.js
+++ Assets/script.js
@@ -3,6 +3,18 @@ function GetInfo() {
     var newName = document.getElementById("city");
     var cityName = document.getElementById("cityName");
     cityName.innerHTML =newName.value;
+    let Key = "777b995858df96d8a63d276827cb2cf5";
 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${newName.value}&units=metric&appid=${Key}`)
+    .then(res => res.json()).then(data => {
+        console.log(data);
+
+    });
+    
 }
+    
+
+    
+    
+   
 
